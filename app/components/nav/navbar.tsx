52d61e5a68ebe5a01e49e67f57ce0e6708de475b
thag15 <2000003730@nttu.edu.vn>
2024-01-12 10:18:49 +0700

    update navbar

--- app/components/nav/navbar.tsx
+++ app/components/nav/navbar.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
+import Container from "../container";
+import { Redressed } from "next/font/google";
+const font = Redressed({ subsets: ["latin"], weight: ["400"] });
 const NavBar = () => {
-    return (<div className="">navbar</div>);
-}
+    return (
+        <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                <Container>
+                    <div className="flex items-center justify-between gap-3 md-gap-0">
+                        <Link href={"/"} className={`${font.className} font-bold text-2xl`}>
+                            Thag
+                        </Link>
+                        <div className="hidden md:block">Search</div>
+                        <div className="flex items-center gap-8 md:gap-12">
+                            <div className="">CartCount</div>
+                            <div className="">UserMenu</div>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        </div>
+    );
+};
 
 export default NavBar;
